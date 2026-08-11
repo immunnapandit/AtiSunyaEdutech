179ee617fd870425ab55416790f29d58c95a9b18
Jayesh Pandit <143267818+jayapandit69@users.noreply.github.com>
2026-08-11 14:54:17 +0530

    course pl

--- components/sections/trusted-partners.tsx
+++ components/sections/trusted-partners.tsx
@@ -10,7 +10,6 @@ type Partner = {
   tagline: string;
   logo: string;
 };
-
 const partners: Partner[] = [
   { name: "Microsoft", tagline: "Cloud Training Services", logo: "/images/logos/microsoft.svg" },
   { name: "Azure", tagline: "Solutions Partner", logo: "/images/logos/azure.svg" },
