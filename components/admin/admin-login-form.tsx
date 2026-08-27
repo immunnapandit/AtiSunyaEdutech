05ce66d282b58bfcc1a37cadcd7c209fb9a4415c
Jayesh Pandit <143267818+jayapandit69@users.noreply.github.com>
2026-08-27 13:19:21 +0530

    REMOVED STRIK THROUGH

--- components/admin/admin-login-form.tsx
+++ components/admin/admin-login-form.tsx
@@ -14,7 +14,6 @@ export function AdminLoginForm() {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
- /*  aSDJYBGFNCSYVEFGISGVEUI NSZNGVE\GFVSUYVLZN */
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     setLoading(true);
